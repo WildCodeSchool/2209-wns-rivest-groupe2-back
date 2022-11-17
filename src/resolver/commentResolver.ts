919f0645b6a701e78d3fe9bd75d7cf1859251f79
Jennifer DE LEO <deleo.jennifer1@gmail.com>
2022-11-17 16:41:11 +0100

    update user crud + create rate & comment crud Co-authored-by: Yan Labarthe <YanLabarthe@users.noreply.github.com> Co-authored-by: charlinedev <charlinedev@users.noreply.github.com>

---
+++ src/resolver/commentResolver.ts
@@ -0,0 +1,120 @@
+import * as argon2 from "argon2";
+import jwt from "jsonwebtoken";
+import { Arg, Field, InputType, Mutation, Query, Resolver } from "type-graphql";
+import { Comment } from "../entities/comment";
+import dataSource from "../utils";
+import { ApolloError } from "apollo-server";
+
+@InputType()
+class CommentType {
+  @Field()
+  userId: int;
+
+  @Field()
+  pointOfInterestId: int;
+
+  @Field()
+  createDate: Date;
+
+  @Field()
+  updateDate: Date;
+
+  @Field()
+  text: string;
+}
+
+@InputType({ description: "Update User data" })
+class UpdateCommentInput {
+  @Field()
+  id: number;
+
+  @Field({ nullable: true })
+  userId: int;
+
+  @Field({ nullable: true })
+  pointOfInterestId: int;
+
+  @Field({ nullable: true })
+  createDate: Date;
+
+  @Field({ nullable: true })
+  updateDate: Date;
+
+  @Field({ nullable: true })
+  text: string;
+}
+
+@Resolver(Comment)
+export class CommentResolver {
+  @Query(() => Comment)
+  async getComment(): Promise<Comment> {
+    return await dataSource.manager.find(Comment);
+      } 
+    } 
+
+  @Query(() => [Comment])
+  async getAllComments(): Promise<Comment[]> {
+    return await dataSource.manager.find(Comment);
+  }
+
+  @Mutation(() => Comment)
+  async createComment(@Arg("data") data: CommentType): Promise<Comment | ApolloError> {
+    const newComment = new Comment();
+    newComment.userId = data.userId;
+    newComment.pointOfInterestId = data.pointOfInterestId;
+    newComment.createDate = data.createDate;
+    newComment.updateDate = data.updateDate;
+    newComment.text = data.text;
+   
+    try {
+      const commentFromDB = await dataSource.manager.save(Comment, newComment);
+      console.log(commentFromDB);
+      return commentFromDB;
+    } catch (err) {
+      throw new ApolloError(err.message);
+    }
+  }
+
+  @Mutation(() => Comment)
+  async updateComment(
+    @Arg("data") data: UpdateCommentInput
+  ): Promise<Comment | ApolloError> {
+    const {
+      id,
+      userId,
+      pointOfInterestId,
+      createDate,
+      updateDate,
+      text,
+    } = data;
+    try {
+      const commentToUpdate = await dataSource.manager.findOneByOrFail(Comment, {
+        id,
+      });
+      username ? (userToUpdate.username = username) : userToUpdate.username;
+      email ? (userToUpdate.email = email) : userToUpdate.email;
+      firstname ? (userToUpdate.firstname = firstname) : userToUpdate.firstname;
+      lastname ? (userToUpdate.lastname = lastname) : userToUpdate.lastname;
+      password
+        ? (userToUpdate.hashedPassword = await argon2.hash(password))
+        : userToUpdate.hashedPassword;
+      profilePicture
+        ? (userToUpdate.profilePicture = profilePicture)
+        : userToUpdate.profilePicture;
+      await dataSource.manager.save(User, userToUpdate);
+      return userToUpdate;
+    } catch (err) {
+      throw new ApolloError(err.message);
+    }
+  }
+
+  @Mutation(() => String)
+  async deleteUser(@Arg("id") id: number): Promise<String> {
+    try {
+      await dataSource.manager.delete(User, { id });
+      return "user deleted";
+    } catch (err) {
+      throw new ApolloError(err.message);
+    }
+  }
+}
